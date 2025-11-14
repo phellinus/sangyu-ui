487f33e7d1b2b553a4b2dfb531637b5fb07ba2fd
桑榆 <2249736997@qq.com>
2025-11-14 09:00:06 +0800

    feat: add eslint + prettier

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+// .prettierrc.js
+/** @type {import('prettier').Config} */
+export default {
+    bracketSpacing: true,
+    singleQuote: true,
+    arrowParens: 'always',
+    trailingComma: 'all',
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    vueIndentScriptAndStyle: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: true,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'ignore',
+    endOfLine: 'auto',
+};
---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+// eslint.config.js
+import js from '@eslint/js';
+import globals from 'globals';
+import configPrettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { defineFlatConfig } from 'eslint-define-config';
+import * as parserTs from '@typescript-eslint/parser';
+import pluginTs from '@typescript-eslint/eslint-plugin';
+import pluginVue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
+
+const { rules: vueRecommendedRules = {} } = pluginVue.configs['vue3-recommended'];
+
+export default defineFlatConfig([
+    {
+        ignores: ['**/.*', 'dist/*'],
+    },
+    {
+        files: ['**/*.{ts,tsx,js,jsx,vue}'],
+        languageOptions: {
+            parser: vueParser,
+            parserOptions: {
+                parser: parserTs,
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                extraFileExtensions: ['.vue'],
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.es2021,
+            },
+        },
+        plugins: {
+            '@typescript-eslint': pluginTs,
+            prettier: pluginPrettier,
+            vue: pluginVue,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...pluginTs.configs.strict.rules,
+            ...vueRecommendedRules,
+            ...configPrettier.rules,
+            'prettier/prettier': [
+                'error',
+                {},
+                {
+                    usePrettierrc: true,
+                    fileInfoOptions: { withNodeModules: true },
+                },
+            ],
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
+    },
+]);
