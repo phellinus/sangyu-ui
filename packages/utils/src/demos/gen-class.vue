4395a7f586aa1c300437980804deacd44d34c0ec
桑榆 <2249736997@qq.com>
2025-11-17 16:42:38 +0800

    feat: 封装className类名生成器

--- packages/utils/src/demos/gen-class.vue
+++ packages/utils/src/demos/gen-class.vue
@@ -1,13 +1,23 @@
 <template>
     <div :class="c('primary')">
         {{ c('primary') }}
+        {{ clx }}
     </div>
+    <button style="background-color: aqua" @click="handleClick">切换</button>
 </template>
 
 <script setup lang="ts">
+    import { ref } from 'vue';
     import { useClassnames } from '../gen-class';
-
-    const { c } = useClassnames('button');
+    const { c, cx } = useClassnames('button');
+    const testRef = ref(false);
+    const handleClick = () => {
+        testRef.value = !testRef.value;
+    };
+    const clx = cx(() => ({
+        c: true,
+        d: testRef.value,
+    }));
 </script>
 
 <style scoped></style>
--- packages/utils/src/gen-class.ts
+++ packages/utils/src/gen-class.ts
@@ -1,10 +1,17 @@
+import classNames from 'classnames';
+import { computed } from 'vue';
+
 export function useClassnames(componentName: string) {
     const prefix = 'sy';
     const componentClass = `${prefix}-${componentName}`;
     const c = (suffix: string) => {
         return `${componentClass}-${suffix}`;
     };
+    const cx = (cls: () => Record<string, boolean>) => {
+        return computed(() => classNames(cls()));
+    };
     return {
         c,
+        cx,
     };
 }
