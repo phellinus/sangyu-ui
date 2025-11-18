a6910527e15c66c86f468092ece14f9fc897c546
桑榆 <2249736997@qq.com>
2025-11-18 11:06:45 +0800

    init:初始化全局组件和样式

--- packages/sangyu-ui/src/index.ts
+++ packages/sangyu-ui/src/index.ts
@@ -1,1 +1,13 @@
-console.log(123);
+import type { App, Plugin } from 'vue';
+import * as components from './components';
+import pkg from '../package.json';
+export default {
+    install(app: App) {
+        Object.entries(components).forEach(([_name, comp]) => {
+            if (comp.install) {
+                app.use(comp as any);
+            }
+        });
+    },
+    version: pkg.version,
+} as Plugin;
