84bcdd0addb8abebed30160429fdac7cc214eaf8
桑榆 <2249736997@qq.com>
2025-12-03 16:24:51 +0800

    feat: label的按钮类型添加

---
+++ packages/sangyu-ui/src/input/demos/label-border-input.vue
@@ -0,0 +1,42 @@
+<docs>
+---
+    title: Input - 基础使用
+---
+
+Input按钮组件的基础使用
+
+</docs>
+
+<template>
+    <div class="content">
+        <SyInput
+            ref="smRef"
+            v-model="value1"
+            type="label-border"
+            placeholder="请输入内容"
+            size="small"
+            bg-color="white"
+        ></SyInput>
+        <SyInput v-model="value2" placeholder="请输入内容" type="label-border" bg-color="white"></SyInput>
+        <SyInput v-model="value" placeholder="请输入内容" type="label-border" size="large"></SyInput>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue';
+    import SyInput from '../input.vue';
+    const value = ref('');
+    const value1 = ref('');
+    const value2 = ref('');
+    const smRef = ref();
+    onMounted(() => {
+        smRef.value.focus();
+    });
+</script>
+<style scoped>
+    .content {
+        display: flex;
+        align-items: flex-start;
+        gap: 10px;
+    }
+</style>
--- packages/sangyu-ui/src/input/index.md
+++ packages/sangyu-ui/src/input/index.md
@@ -1,3 +1,7 @@
 # SyInput 输入框
 
 <demo src="./demos/basic.vue"></demo>
+
+<demo src='./demos/label-input.vue'></demo>
+
+<demo src='./demos/label-border-input.vue'></demo>
