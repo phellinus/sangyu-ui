b7db0fb0c8f18e977fb6c8a35dcf5089add45ac8
桑榆 <2249736997@qq.com>
2025-12-04 18:07:11 +0800

    feat:图标库添加图标

---
+++ packages/icons/src/lightning.vue
@@ -0,0 +1,17 @@
+<template>
+    <svg width="1em" height="1em" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M19 4H37L26 18H41L17 44L22 25H8L19 4Z"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="4"
+            stroke-linejoin="round"
+        />
+    </svg>
+</template>
+
+<script setup lang="ts">
+    defineOptions({
+        name: 'SyLightning',
+    });
+</script>
