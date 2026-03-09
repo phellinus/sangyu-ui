2ac4ec5d8a207217ad88405eeb8af3a0bc05d26e
桑榆 <2249736997@qq.com>
2026-03-09 23:38:31 +0800

    feat:notification添加关闭功能

--- packages/sangyu-ui/src/notification/notification.tsx
+++ packages/sangyu-ui/src/notification/notification.tsx
@@ -9,25 +9,27 @@ export default defineComponent<{
 	setup(props, { expose }) {
 		const data = ref<NotificationConfigType[]>([]);
 		let index = 0;
+		// 抽离统一关闭方法
+		const remove = (_id: number | string) => {
+			const targetIndex = data.value.findIndex((item) => item._id === _id);
+			if (targetIndex !== -1) {
+				const target = data.value[targetIndex];
+				if (target._timer) {
+					clearTimeout(target._timer);
+				}
+				data.value.splice(targetIndex, 1);
+			}
+		};
 		const add = (config: NotificationConfig) => {
 			const instance: NotificationConfigType = {
 				...config,
-				_id: Date.now() + index,
-			};
-			//关闭当前niotification
-			const close = () => {
-				const index = data.value.findIndex((item) => item._id === instance._id);
-				if (index !== -1) {
-					data.value.splice(index, 1);
-					if (instance._timer) {
-						clearTimeout(instance._timer);
-					}
-				}
+				showClose: config.showClose ?? true,
+				_id: Date.now() + index++,
 			};
 			//如果没有设置为时间为0，直接默认3秒销毁
 			if (instance.durnation !== 0) {
 				instance._timer = setTimeout(() => {
-					close();
+					remove(instance._id);
 				}, instance.durnation ?? 3000);
 			}
 			data.value.push(instance);
@@ -46,6 +48,9 @@ export default defineComponent<{
 		expose({ add });
 		return () => {
 			const renderNotification = () => {
+				const noCls = {
+					[c('notify')]: true,
+				};
 				const cls = {
 					[c('wrapper')]: true,
 				};
@@ -57,9 +62,24 @@ export default defineComponent<{
 				};
 				return data.value.map((item) => {
 					return (
-						<div class={cls} key={item._id}>
-							<div class={titleCls}>{item.title}</div>
-							<div class={contentCls}>{item.content}</div>
+						<div class={noCls} key={item._id}>
+							{/* <div>图标</div> */}
+							<div class={cls}>
+								<div class={titleCls}>{item.title}</div>
+								<div class={contentCls}>{item.content}</div>
+							</div>
+							{item.showClose && (
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									viewBox='0 0 640 640'
+									width='16'
+									height='16'
+									class={'sy-notification-close'}
+									onClick={() => remove(item._id)}
+								>
+									<path d='M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z' />
+								</svg>
+							)}
 						</div>
 					);
 				});
