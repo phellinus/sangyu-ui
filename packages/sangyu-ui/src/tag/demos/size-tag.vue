278de374b895bab36f980319f601692863ed4eaf
桑榆 <2249736997@qq.com>
2026-03-13 21:58:36 +0800

    feat:tag尺寸用法文档添加

---
+++ packages/sangyu-ui/src/tag/demos/size-tag.vue
@@ -0,0 +1,31 @@
+<docs>
+---
+title: SyTag的尺寸用法
+---
+SyTag组件提供了三种尺寸，可以通过`size`属性来设置。default为默认尺寸，small为小尺寸，large为更大尺寸。
+</docs>
+
+<template>
+	<div style="display: flex; align-items: center; gap: 20px">
+		<SyTag v-for="(item, index) in sizeTag" :key="index" :type="item.type" :size="item.size">{{ item.size }}</SyTag>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	import { SyTag } from 'sangyu-ui';
+
+	const sizeTag = [
+		{
+			size: 'small' as any,
+			type: 'primary',
+		},
+		{
+			size: 'default' as any,
+			type: 'success',
+		},
+		{
+			size: 'large' as any,
+			type: 'warning',
+		},
+	];
+</script>
--- packages/sangyu-ui/src/tag/index.md
+++ packages/sangyu-ui/src/tag/index.md
@@ -2,3 +2,6 @@
 
 ## 基础用法
 <demo src="./demos/basic-tag.vue"></demo>
+
+## 尺寸用法
+<demo src="./demos/size-tag.vue"></demo>
