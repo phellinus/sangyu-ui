da3dd26b0b4f0380e8a0a6f350f25587dbac1f49
桑榆 <2249736997@qq.com>
2026-03-15 21:09:53 +0800

    feat:添加面包屑组件样式

--- packages/sangyu-ui/src/breadcrumb/demos/separator-breadcrumb.vue
+++ packages/sangyu-ui/src/breadcrumb/demos/separator-breadcrumb.vue
@@ -1,14 +1,14 @@
 <docs>
 ---
-title: SyBreadCrumb 面包屑基本用法
+title: SyBreadCrumb 分隔符
 ---
 
-SyBreadCrumb 面包屑基本用法
+SyBreadCrumb通过设置separator属性可以自定义分隔符，默认分隔符为"/"，也可以通过separatorIcon属性设置分隔符图标。
 </docs>
 
 <template>
 	<div>
-		<SyBreadCrumb>
+		<SyBreadCrumb separator=">">
 			<SyBreadcrumbItem to="/">首页</SyBreadcrumbItem>
 			<SyBreadcrumbItem to="/component">组件库</SyBreadcrumbItem>
 			<SyBreadcrumbItem to="/component/breadcrumb">面包屑</SyBreadcrumbItem>
--- packages/sangyu-ui/src/breadcrumb/index.md
+++ packages/sangyu-ui/src/breadcrumb/index.md
@@ -1,3 +1,5 @@
 # Breadcrumb 面包屑
 
 <demo src="./demos/basic-breadcrumb.vue"></demo>
+
+<demo src="./demos/separator-breadcrumb.vue"></demo>
