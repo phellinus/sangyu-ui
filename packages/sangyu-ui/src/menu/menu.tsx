c8d658d14c7bacedd98debd50ae1103c828d9845
桑榆 <2249736997@qq.com>
2026-03-16 22:41:44 +0800

    feat:添加相关样式

--- packages/sangyu-ui/src/menu/menu.tsx
+++ packages/sangyu-ui/src/menu/menu.tsx
@@ -1,13 +1,64 @@
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
 import { MenuProps } from './interface';
+import { useClassnames } from '@sangyu-ui/utils';
 
-export default defineComponent(
-	(props: MenuProps, { slots }) => {
+export default defineComponent({
+	name: 'SyMenu',
+	props: {
+		defaultIndex: {
+			type: String,
+			default: '',
+		},
+		mode: {
+			type: String as PropType<MenuProps['mode']>,
+			default: 'vertical',
+		},
+		hoverBgColor: {
+			type: String,
+			default: '',
+		},
+		hoverColor: {
+			type: String,
+			default: '',
+		},
+		customStyle: {
+			type: String,
+			default: '',
+		},
+		verticalPosition: {
+			type: String as PropType<MenuProps['verticalPosition']>,
+			default: 'left',
+		},
+		itemPosition: {
+			type: String as PropType<MenuProps['itemPosition']>,
+			default: 'left',
+		},
+		expand: {
+			type: Boolean,
+			default: true,
+		},
+		defaultOpenSubMenus: {
+			type: Array as PropType<string[]>,
+			default: () => [],
+		},
+		onSelect: {
+			type: Function as PropType<(selectedIndex: string) => void>,
+		},
+	},
+	setup(props, { slots }) {
+		const { c } = useClassnames('menu');
+		const menuCls = {
+			[c()]: true,
+			[c(props.mode)]: true,
+			[c(props.verticalPosition)]: true,
+		};
+
 		return () => {
-			return <div>菜单项</div>;
+			return (
+				<ul class={menuCls} style={props.customStyle}>
+					{slots.default?.()}123
+				</ul>
+			);
 		};
 	},
-	{
-		name: 'SyMenu',
-	},
-);
+});
