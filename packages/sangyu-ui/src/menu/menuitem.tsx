4271be0c038a88bf9a641ab88186607a20c47b8f
桑榆 <2249736997@qq.com>
2026-03-16 22:46:03 +0800

    feat:submenu和menuitem子组件添加绑定样式

--- packages/sangyu-ui/src/menu/menuitem.tsx
+++ packages/sangyu-ui/src/menu/menuitem.tsx
@@ -1,10 +1,15 @@
 import { defineComponent } from 'vue';
 import { MenuItemProps } from './interface';
+import { useClassnames } from '@sangyu-ui/utils';
 
 export default defineComponent(
 	(props: MenuItemProps, { slots }) => {
+		const { c } = useClassnames('menu-item');
+		const menuItemCls = {
+			[c()]: true,
+		};
 		return () => {
-			return <div>123</div>;
+			return <div class={menuItemCls}>{slots.default?.()}</div>;
 		};
 	},
 	{
--- packages/sangyu-ui/src/menu/submenu.tsx
+++ packages/sangyu-ui/src/menu/submenu.tsx
@@ -1,10 +1,19 @@
 import { defineComponent } from 'vue';
 import { SubMenuProps } from './interface';
+import { useClassnames } from '@sangyu-ui/utils';
 
 export default defineComponent(
 	(props: SubMenuProps, { slots }) => {
+		const { c } = useClassnames('submenu');
+		const submenuCls = {
+			[c()]: true,
+		};
 		return () => {
-			return <div>123</div>;
+			return (
+				<div class={submenuCls} style={props.customStyle}>
+					{slots.default?.()}
+				</div>
+			);
 		};
 	},
 	{
