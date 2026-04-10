dddffe5b96cda291168c0ae37b3aa42336a153d0
桑榆 <2249736997@qq.com>
2026-04-10 23:47:02 +0800

    feat:完善step组件的逻辑

--- packages/sangyu-ui/src/step/step.tsx
+++ packages/sangyu-ui/src/step/step.tsx
@@ -1,16 +1,130 @@
-import { defineComponent } from 'vue';
+import { computed, defineComponent, inject, PropType, Ref } from 'vue';
+import { useClassnames } from '@sangyu-ui/utils';
+import { SyIcon } from '@sangyu-ui/icons';
+import { StepDirection, StepProps, StepStatus } from './interface';
+import { stepsKey } from './steps';
 
-export default defineComponent(
-	(props, { slots }) => {
+export default defineComponent({
+	name: 'SyStep',
+	props: {
+		title: {
+			type: String,
+			default: '',
+		},
+		description: {
+			type: String,
+			default: '',
+		},
+		icon: {
+			type: String,
+			default: '',
+		},
+		status: {
+			type: String as PropType<StepProps['status']>,
+			default: undefined,
+		},
+		index: {
+			type: Number,
+			default: -1,
+		},
+		total: {
+			type: Number,
+			default: 0,
+		},
+		customStyle: {
+			type: String,
+			default: '',
+		},
+	},
+	setup(props, { slots }) {
+		const stepsContext = inject<{
+			active: Ref<number>;
+			direction: Ref<StepDirection>;
+		}>(stepsKey, {
+			active: computed(() => 0),
+			direction: computed(() => 'horizontal'),
+		});
+
+		const { c } = useClassnames('step');
+		const direction = computed(() => stepsContext.direction?.value || 'horizontal');
+		const resolvedStatus = computed<StepStatus>(() => {
+			if (props.status) {
+				return props.status;
+			}
+
+			const active = stepsContext.active?.value ?? 0;
+
+			if (props.index < active) {
+				return 'finish';
+			}
+
+			if (props.index === active) {
+				return 'process';
+			}
+
+			return 'wait';
+		});
+		const isLast = computed(() => props.index === props.total - 1);
+		const hasDescription = computed(() => !!(slots.description?.() || props.description));
+
+		const getStepCls = () => ({
+			[c()]: true,
+			[c(direction.value)]: true,
+			[c(resolvedStatus.value)]: true,
+			[c('with-description')]: hasDescription.value,
+			'is-last': isLast.value,
+		});
+		const getIndicatorCls = () => ({
+			[c('indicator')]: true,
+			[c(`indicator-${resolvedStatus.value}`)]: true,
+		});
+		const getTailCls = () => ({
+			[c('tail')]: true,
+			[c(`tail-${resolvedStatus.value}`)]: true,
+		});
+
+		const renderIndicatorContent = () => {
+			if (slots.icon) {
+				return slots.icon();
+			}
+
+			if (props.icon) {
+				return <SyIcon name={props.icon} size={18} />;
+			}
+
+			if (resolvedStatus.value === 'finish') {
+				return <SyIcon name='check' size={16} />;
+			}
+
+			if (resolvedStatus.value === 'error') {
+				return <SyIcon name='close' size={16} />;
+			}
+
+			return <span class={c('index')}>{props.index >= 0 ? props.index + 1 : ''}</span>;
+		};
+
 		return () => {
+			const titleNode = slots.title?.() ?? props.title;
+			const descriptionNode = slots.description?.() ?? props.description;
+
 			return (
-				<>
-					<div>step</div>
-				</>
+				<div
+					class={getStepCls()}
+					style={props.customStyle}
+					data-index={props.index}
+					data-status={resolvedStatus.value}
+					data-direction={direction.value}
+				>
+					<div class={c('head')}>
+						<div class={getIndicatorCls()}>{renderIndicatorContent()}</div>
+						{!isLast.value && <div class={getTailCls()}></div>}
+					</div>
+					<div class={c('main')}>
+						<div class={c('title')}>{titleNode}</div>
+						{descriptionNode && <div class={c('description')}>{descriptionNode}</div>}
+					</div>
+				</div>
 			);
 		};
 	},
-	{
-		name: 'SyStep',
-	},
-);
+});
--- packages/sangyu-ui/src/step/steps.tsx
+++ packages/sangyu-ui/src/step/steps.tsx
@@ -1,12 +1,89 @@
-import { defineComponent } from 'vue';
+import { cloneVNode, Comment, defineComponent, Fragment, PropType, provide, toRef, VNode } from 'vue';
+import { useClassnames } from '@sangyu-ui/utils';
+import { StepDirection, StepsProps } from './interface';
 
-export default defineComponent(
-	(props, { slots }) => {
+export const stepsKey = Symbol('stepsKey');
+
+const flattenChildren = (children: any[]): VNode[] => {
+	const result: VNode[] = [];
+
+	children.forEach((child) => {
+		if (!child) {
+			return;
+		}
+
+		if (Array.isArray(child)) {
+			result.push(...flattenChildren(child));
+			return;
+		}
+
+		if (child.type === Fragment && Array.isArray(child.children)) {
+			result.push(...flattenChildren(child.children as any[]));
+			return;
+		}
+
+		if (child.type !== Comment) {
+			result.push(child);
+		}
+	});
+
+	return result;
+};
+
+export default defineComponent({
+	name: 'SySteps',
+	props: {
+		active: {
+			type: Number,
+			default: 0,
+		},
+		direction: {
+			type: String as PropType<StepsProps['direction']>,
+			default: 'horizontal',
+		},
+		customStyle: {
+			type: String,
+			default: '',
+		},
+	},
+	setup(props, { slots }) {
+		provide(stepsKey, {
+			active: toRef(props, 'active'),
+			direction: toRef(props, 'direction') as any,
+		});
+
+		const { c } = useClassnames('steps');
+		const getStepsCls = () => ({
+			[c()]: true,
+			[c(props.direction as StepDirection)]: true,
+		});
+
 		return () => {
-			return <div class='sy-steps'>{slots.default?.()}</div>;
+			const children = flattenChildren(slots.default?.() || []);
+			const validChildren = children.filter((child) => {
+				const componentName = (child.type as any)?.name;
+				const isValid = componentName === 'SyStep';
+
+				if (!isValid) {
+					console.warn('[SySteps] 子组件只能是 SyStep');
+				}
+
+				return isValid;
+			});
+
+			const total = validChildren.length;
+			const items = validChildren.map((child, index) =>
+				cloneVNode(child, {
+					index,
+					total,
+				}),
+			);
+
+			return (
+				<div class={getStepsCls()} style={props.customStyle}>
+					{items}
+				</div>
+			);
 		};
 	},
-	{
-		name: 'SySteps',
-	},
-);
+});
