b0e08aeef9058d7c7a3aeba188aaf48b84ffe3de
桑榆 <2249736997@qq.com>
2026-06-17 23:17:53 +0800

    feat:syradio的基础实现

--- packages/sangyu-ui/src/radio/demos/basic-radio.vue
+++ packages/sangyu-ui/src/radio/demos/basic-radio.vue
@@ -3,13 +3,21 @@
 title: SyRadio 的基础用法
 ---
 
-
+通过设置 `size` 可以设置不同尺寸的 `SyRadio`，通过设置 `shape` 可以设置不同形状的 `SyRadio`
 </docs>
 
 <template>
 	<div>
-		<sy-radio v-model="value" label="1">Option 1</sy-radio>
-		<sy-radio v-model="value2" label="1" shape="square">Option 2</sy-radio>
+		<div class="radio-inline">
+			<sy-radio v-model="value" size="small" label="1">Option 1</sy-radio>
+			<sy-radio v-model="value" label="1">Option 1</sy-radio>
+			<sy-radio v-model="value" size="large" label="1">Option 1</sy-radio>
+		</div>
+		<div class="radio-inline">
+			<sy-radio v-model="value2" label="1" size="small" shape="square">Option 4</sy-radio>
+			<sy-radio v-model="value2" label="1" shape="square">Option 5</sy-radio>
+			<sy-radio v-model="value2" label="1" size="large" shape="square">Option 6</sy-radio>
+		</div>
 	</div>
 </template>
 
@@ -18,5 +26,13 @@ title: SyRadio 的基础用法
 	import { ref } from 'vue';
 
 	const value = ref(false);
-	const value2 = ref(true);
+	const value2 = ref(false);
 </script>
+
+<style scoped>
+	.radio-inline {
+		margin: 10px;
+		display: flex;
+		gap: 16px;
+	}
+</style>
