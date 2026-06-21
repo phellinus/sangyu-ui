79f398ae17dfecb410a2ad0dfec9f8448a876b36
桑榆 <2249736997@qq.com>
2026-06-21 23:19:29 +0800

    feat:基础文档的实现

---
+++ packages/sangyu-ui/src/checkbox/demos/basic-checkbox.vue
@@ -0,0 +1,20 @@
+<docs>
+---
+title: sy-checkbox 的基础用法
+---
+
+</docs>
+
+<template>
+	<div>
+		<sy-checkbox v-model="checked" label="复选框 A"></sy-checkbox>
+		<sy-checkbox v-model="checked" label="复选框 B"></sy-checkbox>
+		<sy-checkbox v-model="checked" label="复选框 C"></sy-checkbox>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { ref } from 'vue';
+	import { SyCheckbox } from 'sangyu-ui';
+	const checked = ref(true);
+</script>
---
+++ packages/sangyu-ui/src/checkbox/index.md
@@ -0,0 +1,5 @@
+# SyCheckBox 多选框
+
+## 基础用法
+
+<demo src="./demos/basic-checkbox.vue"></demo>
