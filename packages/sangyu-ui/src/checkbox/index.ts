6587c401ca692daf2a4eb3badb26bcc8b56f99de
桑榆 <2249736997@qq.com>
2026-06-22 00:18:17 +0800

    feat:注册sycheckgroup

--- packages/sangyu-ui/src/checkbox/index.ts
+++ packages/sangyu-ui/src/checkbox/index.ts
@@ -1,7 +1,9 @@
 import { App } from 'vue';
 import SyCheckbox from './SyCheckbox';
+import SyCheckboxGroup from './SyCheckboxGroup';
 (SyCheckbox as any).install = (app: App) => {
 	app.component(SyCheckbox.name || 'SyCheckbox', SyCheckbox);
+	app.component(SyCheckboxGroup.name || 'SyCheckboxGroup', SyCheckboxGroup);
 };
 export { SyCheckbox };
 export * from './Checkbox.types';
