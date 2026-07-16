a4d1b163d997dba99afe7055953f8241b2872383
桑榆 <2249736997@qq.com>
2026-07-16 23:47:31 +0800

    feat:修改website的UI样式

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,21 +1,23 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
-import { AntdTheme } from 'vite-plugin-vitepress-demo/theme'
-import sangyu from "sangyu-ui" 
-import 'sangyu-ui/style'
+import { h } from 'vue';
+import type { Theme } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import './style.css';
+import { AntdTheme } from 'vite-plugin-vitepress-demo/theme';
+import sangyu from 'sangyu-ui';
+import 'sangyu-ui/style';
+import HomeReveal from './HomeReveal.vue';
+
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-    app.component('Demo', AntdTheme)
-    app.use(sangyu)
-  }
-} satisfies Theme
+	extends: DefaultTheme,
+	Layout: () => {
+		return h(DefaultTheme.Layout, null, {
+			'home-hero-before': () => h(HomeReveal),
+		});
+	},
+	enhanceApp({ app, router, siteData }) {
+		// ...
+		app.component('Demo', AntdTheme);
+		app.use(sangyu);
+	},
+} satisfies Theme;
