5b3769054c836f29148373dc22ff34516ce0010b
桑榆 <2249736997@qq.com>
2026-08-02 23:27:20 +0800

    feat:实现表单功能的校验的函数抽离

--- packages/sangyu-ui/src/form/composable/index.ts
+++ packages/sangyu-ui/src/form/composable/index.ts
@@ -1,2 +1,4 @@
 export * from './useFormItemContext';
 export * from './useFormContext';
+export * from './useValidation';
+export * from './useFormItem';
--- packages/sangyu-ui/src/form/composable/useValidation.ts
+++ packages/sangyu-ui/src/form/composable/useValidation.ts
@@ -1,5 +1,6 @@
 import { computed, ref, Ref } from 'vue';
-import { FormRule, ValidateStatus } from '../Form.type';
+import { FormRule, ValidateStatus, ValidateTrigger } from '../Form.type';
+import { matchesTrigger, validateValue } from '../utils';
 
 interface UseValidationOptions {
 	name: string;
@@ -26,6 +27,60 @@ export function useValidation(options: UseValidationOptions) {
 	const validateStatus = ref<ValidateStatus>('');
 	// 验证版本
 	let validateVersion = 0;
-
+	// 当前字段校验规则是否有错误
 	const hasError = computed(() => errors.value.length > 0);
+	// 当前字段的校验状态
+	const validate = async (trigger?: ValidateTrigger) => {
+		const version = ++validateVersion;
+
+		const rules = options.getRules().filter((rule) => matchesTrigger(rule, trigger));
+
+		if (!rules.length) {
+			errors.value = [];
+			validateStatus.value = '';
+			return;
+		}
+
+		validating.value = true;
+		validateStatus.value = 'validating';
+
+		const nextErrors = await validateValue({
+			name: options.name,
+			label: options.label,
+			value: options.value.value,
+			rules,
+			model: options.model,
+		});
+
+		if (version !== validateVersion) return;
+
+		errors.value = nextErrors;
+		validating.value = false;
+		validateStatus.value = nextErrors.length ? 'error' : 'success';
+
+		options.onValidated?.(validateStatus.value, errors.value);
+
+		if (nextErrors.length) {
+			throw nextErrors;
+		}
+	};
+	/**
+	 * 清除当前字段的校验状态和信息
+	 */
+	const clearValidate = () => {
+		validateVersion += 1;
+		errors.value = [];
+		validating.value = false;
+		validateStatus.value = '';
+	};
+	return {
+		errors,
+		hasError,
+		validating,
+		touched,
+		dirty,
+		validateStatus,
+		validate,
+		clearValidate,
+	};
 }
