eacbde17169d286e8b8cdb9dfb68afa701cd8ce3
桑榆 <2249736997@qq.com>
2026-08-11 23:13:38 +0800

    feat:抽屉基础示例搭建

---
+++ packages/sangyu-ui/src/drawer/demos/basic-drawer.vue
@@ -0,0 +1,17 @@
+<docs>
+---
+title: 基础用法
+---
+
+通过 `visible` 控制抽屉开关。
+</docs>
+
+<template>
+	<div>
+		<SyDrawer />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { SyDrawer } from 'sangyu-ui';
+</script>
--- packages/sangyu-ui/src/drawer/index.md
+++ packages/sangyu-ui/src/drawer/index.md
@@ -1,1 +1,5 @@
 # Drawer 抽屉
+
+## 基础抽屉
+
+<demo src="./demos/basic-drawer.vue"></demo>
