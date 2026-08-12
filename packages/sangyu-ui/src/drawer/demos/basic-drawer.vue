1e76c4759998705933d2d923e5d88292baca9975
桑榆 <2249736997@qq.com>
2026-08-12 23:55:14 +0800

    feat:基础drawer的示例的完善

--- packages/sangyu-ui/src/drawer/demos/basic-drawer.vue
+++ packages/sangyu-ui/src/drawer/demos/basic-drawer.vue
@@ -3,15 +3,20 @@
 title: 基础用法
 ---
 
-通过 `visible` 控制抽屉开关。
+通过 `visible` 控制抽屉开关
 </docs>
 
 <template>
 	<div>
-		<SyDrawer />
+		<SyButton @click="visible = true">打开抽屉</SyButton>
+		<SyDrawer v-model:visible="visible" title="基础抽屉" :width="360">这里是抽屉内容</SyDrawer>
 	</div>
 </template>
 
 <script lang="ts" setup>
-import { SyDrawer } from 'sangyu-ui';
+import { ref } from 'vue';
+import { SyButton, SyDrawer } from 'sangyu-ui';
+
+// 控制抽屉显示状态
+const visible = ref(false);
 </script>
